0dcc5fa39fd13c7010edbf2db92a498f7af93007
shogonir <shogonir@gmail.com>
2016-05-19 00:21:23 +0900

    AutoJcon.parse_table() implemented

--- AutoJcon.py
+++ AutoJcon.py
@@ -1,15 +1,30 @@
 #! /usr/bin/env python
 # -*- coding:utf-8 -*-
 
+import re
+
 class AutoJcon :
     
     def __init__ (self) :
-        pass
+        self.html = ''
 
     def read_html (self, fname='table.html') :
+        if self.html != '' :
+            return
         for line in open(fname, 'r') :
-            print line,
+            self.html += line
+
+    def parse_table (self) :
+        tmp_html = self.html.replace('\n', '')
+        print tmp_html
+        pattern_tr = re.compile(r"<tr.*?tr>");
+        for match_string in pattern_tr.findall(tmp_html) :
+            print 'matches', match_string
+            print
 
 if __name__ == '__main__' :
     auto_jcon = AutoJcon()
     auto_jcon.read_html()
+    print auto_jcon.html
+    auto_jcon.parse_table()
+
